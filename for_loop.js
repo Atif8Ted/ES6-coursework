192442b7a4340e3bf7d04dfa1503f0e25bef9e9b
Atif8Ted <mohd.atifimam@hotmail.com>
2017-10-15 02:12:22 +0530

    addded

---
+++ for_loop.js
@@ -0,0 +1,7 @@
+for(var i=0;i<10;i++){
+  console.log(i);
+}
+var names=["Aap","BJP","Congress"];
+for(var i=0;i<names.length;i++){
+  console.log(names[i]);
+}
---
+++ while_loop.js
@@ -0,0 +1,16 @@
+var names = ["Aap", "BJP", "Congress"];
+var i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
+//do while_loop
+var i=0;
+do{
+  console.log(names[i]);
+  i++;
+}
+while (i<names.length) {
+  console.log(names[i]);
+
+}
