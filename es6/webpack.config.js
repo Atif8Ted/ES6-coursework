95a9c39f0ac685052e75ae69d15eec0f3a20318e
Atif8Ted <mohd.atifimam@hotmail.com>
2017-10-15 02:39:13 +0530

    added

---
+++ es6/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports={
+  entry:['/home/atif/Documents/es6 in depth/es6/app/index.js'],
+  output:{
+    path:'/home/atif/Documents/es6 in depth/es6/build',
+    filename:'bundle.js'
+  }
+}
